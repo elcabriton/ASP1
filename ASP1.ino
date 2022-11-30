e519e8b0aa66728f96ff9d8403428153dc8b64d1
Heitor <mchheitor@gmail.com>
2022-11-30 01:23:37 -0300

    att

--- ASP1.ino
+++ ASP1.ino
@@ -8,6 +8,7 @@ int pinG = 5;
 int pinDP = 6;
 int verde = 12;
 int contador=0;
+int count=0;    //TIMER
 int interrupcao=2;//INT0
 
 void setup() {
@@ -34,11 +35,11 @@ void setup() {
 
   
   //TIMER
-  // TCCR1A = 0;            //configura timer para operação normal pinos OC1A e OC1B desconectados
-  // TCCR1B = 0;            //limpa registrador
-  // TIMSK1 |= (1 << TOIE1);// habilita a interrupção do TIMER1
-  // TCCR1B = 1;            // modo normal sem prescaler
-  // TCNT1 = 0;             //SEM PRESCALER
+  TCCR1A = 0;            //configura timer para operação normal pinos OC1A e OC1B desconectados
+  TCCR1B = 0;            //limpa registrador
+  TIMSK1 |= (1 << TOIE1);// habilita a interrupção do TIMER1
+  TCCR1B = 1;            // modo normal sem prescaler
+  TCNT1 = 0;             //SEM PRESCALER
   // 65536 ciclos * 6.25e-08 (periodo do ciclo em s) =  0.004096 s = 4.09ms tempo para interrupcao
   // 10000 ms / 4.09 ms/interrupcao = +-245 
   // 245 ciclos teremos 1S  
@@ -50,20 +51,18 @@ void setup() {
 void status();
 void loop() {
       status();
-      // digitalWrite(pinA, HIGH);
-      // digitalWrite(pinB, HIGH);
-      // digitalWrite(pinC, HIGH);
-      // digitalWrite(pinD, HIGH);
-      // digitalWrite(pinE, HIGH);
-      // digitalWrite(pinF, HIGH);
-      // digitalWrite(pinG, HIGH);
-      // digitalWrite(pinDP, HIGH);
+
       
 }
 void botaoAcionado()
 {
-  contador++;
+  static unsigned long delayEstado;
+  if((millis()-delayEstado)>1000){
+  contador--;
+  delayEstado=millis();
+  }
 }
+
   
   
   
@@ -71,15 +70,49 @@ void botaoAcionado()
       
  
 
-// ISR(TIMER1_OVF_vect)//interrupção do TIMER1 
-// { 
-   
-//   TCNT1=0;
-//   contador++;
+ISR(TIMER1_OVF_vect)//interrupção do TIMER1 
+{ 
+   if(count==245){//H
+     contador++;
+   }
+   if(count==490){//E
+     contador++;
+   }
+   if(count==735){//L
+     contador++;
+   }
+   if(count==980){//L
+     contador++;
+   }
+   if(count==1225){//O
+     contador++;
+   }
+   if(count==1470){//A
+     contador++;
+   }
+   if(count==1715){//T
+     contador++;
+   }
+   if(count==1960){//3
+     contador++;
+   }
+   if(count==2205){//2
+     contador++;
+   }
+   if(count==2450){//8
+     contador++;
+   }
+   if(count==2695){//P
+     contador++;
+   }
 
   
 
-// }
+  
+
+  count++;
+  TCNT1=0;
+}
 
 
 
@@ -108,7 +141,7 @@ void status(){
       digitalWrite(pinG, HIGH);
       digitalWrite(pinDP,LOW);
       
-      digitalWrite(verde,HIGH);
+      
   }
   if(contador==2){//E
       digitalWrite(pinA,HIGH);
@@ -120,7 +153,7 @@ void status(){
       digitalWrite(pinG, HIGH);
       digitalWrite(pinDP,LOW);
       
-      digitalWrite(verde,LOW);
+      
   }
   if(contador==3){//L
       digitalWrite(pinA,LOW);
@@ -179,7 +212,7 @@ void status(){
         digitalWrite(pinD, LOW);
         digitalWrite(pinE, LOW);
         digitalWrite(pinF, LOW);
-        digitalWrite(pinG, HIGH);
+        digitalWrite(pinG, LOW);
         digitalWrite(pinDP,LOW);
       
       
@@ -189,7 +222,7 @@ void status(){
         digitalWrite(pinA,HIGH);
         digitalWrite(pinB, HIGH);
         digitalWrite(pinC, HIGH);
-        digitalWrite(pinD, LOW);
+        digitalWrite(pinD, HIGH);
         digitalWrite(pinE, LOW);
         digitalWrite(pinF, LOW);
         digitalWrite(pinG, HIGH);
@@ -234,21 +267,9 @@ void status(){
         digitalWrite(pinF, HIGH);
         digitalWrite(pinG, HIGH);
         digitalWrite(pinDP,LOW);
-      
+        contador=-1; 
       
       
   }
-   if(contador==12){//T
-        digitalWrite(pinA,LOW);
-        digitalWrite(pinB, LOW);
-        digitalWrite(pinC, LOW);
-        digitalWrite(pinD, LOW);
-        digitalWrite(pinE, LOW);
-        digitalWrite(pinF, LOW);
-        digitalWrite(pinG, LOW);
-        digitalWrite(pinDP,LOW);
-      
-        contador=-1;  
-      
-  }
+  
 }
